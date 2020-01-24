f53d576c1b23935977a8c66558fcedd7e3e09d91
BodhServant <51540912+BodhServant@users.noreply.github.com>
2020-01-24 23:40:37 +0700

    Update cal-data.js

--- js/data/cal-data.js
+++ js/data/cal-data.js
@@ -438,6 +438,53 @@ var calendarData = [
         "end": "",
         "url": "",
         "visible": true
+    },
+    {
+        "id": 3,
+        "lastUpdate": "2019-06-08T18:42:29.000+0000",
+        "temple_id": 1,
+        "groupId": 0,
+        "title": "วันพระ",
+        "start": "2020-02-01",
+        "end": "",
+        "url": "",
+        "visible": true
+    }
+    ,
+    {
+        "id": 3,
+        "lastUpdate": "2019-06-08T18:42:29.000+0000",
+        "temple_id": 1,
+        "groupId": 0,
+        "title": "วันพระ",
+        "start": "2020-02-08",
+        "end": "",
+        "url": "",
+        "visible": true
+    }
+    ,
+    {
+        "id": 3,
+        "lastUpdate": "2019-06-08T18:42:29.000+0000",
+        "temple_id": 1,
+        "groupId": 0,
+        "title": "วันพระ",
+        "start": "2020-02-16",
+        "end": "",
+        "url": "",
+        "visible": true
+    }
+    ,
+    {
+        "id": 3,
+        "lastUpdate": "2019-06-08T18:42:29.000+0000",
+        "temple_id": 1,
+        "groupId": 0,
+        "title": "วันพระ",
+        "start": "2020-02-22",
+        "end": "",
+        "url": "",
+        "visible": true
     }
     
 ];
